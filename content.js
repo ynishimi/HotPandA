b7b8268e68e9a770e7084930e3f579871063da23
ynishimi <nishimi.yuki.37z@st.kyoto-u.ac.jp>
2024-01-16 20:55:08 +0900

    タブがアクティブな時のみ自動で遷移し、それ以外の時はボタンで遷移するように変更

--- content.js
+++ content.js
@@ -1,13 +1,13 @@
 const url = "https://panda.ecs.kyoto-u.ac.jp/cas/login?service=https%3A%2F%2Fpanda.ecs.kyoto-u.ac.jp%2Fsakai-login-tool%2Fcontainer";
-// const url2 = "https://panda.ecs.kyoto-u.ac.jp/portal/login";
-// let loginButton = document.createElement("button");
-// loginButton.textContent = "ログイン";
-// loginButton.setAttribute("onclick", "location.href='${url2}'")
-// let target = document.querySelector("#msg");
-// target.appendChild(loginButton);
+function goLogin() {
+  location.href = url;
+}
+タブがアクティブな時に自動で遷移
+window.addEventListener("focus", goLogin());
 
-// window.onload = function() {
-//     location.href = url;
-// }
-
-window.addEventListener('pageshow', () => location.href = url);
+// ログインボタン
+let loginButton = document.createElement("button");
+loginButton.textContent = "ログイン";
+loginButton.onclick = goLogin;
+let target = document.querySelector("#msg");
+target.appendChild(loginButton);
